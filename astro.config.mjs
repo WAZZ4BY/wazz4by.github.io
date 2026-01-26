a4e9d99d1843c18b73d2d4816e32c7b7adbbc07d
WAZZ4BY <wass4lsnow@gmail.com>
2026-01-26 23:28:24 +0300

    2601 2328

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// Для user/organization pages (username.github.io) base должен быть '/'
-// Для project pages (username.github.io/repo-name) base должен быть '/repo-name'
 export default defineConfig({
+  site: 'https://vasilyaleev.ru',
   output: 'static',
-  site: 'https://wazz4by.github.io',
-  base: '/',
 });
